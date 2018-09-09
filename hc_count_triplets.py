3e621ae9fc90a915a38d705f8017bf832a451ede
Vincent Teng <vincentt117@gmail.com>
2018-09-09 13:38:09 -0400

    Added an LC challenge

---
+++ hc_count_triplets.py
@@ -0,0 +1,38 @@
+# https://www.hackerrank.com/challenges/count-triplets-1/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=dictionaries-hashmaps
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+import collections
+
+# Complete the countTriplets function below.
+def countTriplets(arr, r):
+    v3 = collections.defaultdict(int)
+    v2 = collections.defaultdict(int)
+    count = 0
+    for k in arr:
+        count += v3[k]
+        v3[k*r] += v2[k]
+        v2[k*r] += 1
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nr = input().rstrip().split()
+
+    n = int(nr[0])
+
+    r = int(nr[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    ans = countTriplets(arr, r)
+
+    fptr.write(str(ans) + '\n')
+
+    fptr.close()
---
+++ lc_isomorphic_strings.py
@@ -0,0 +1,26 @@
+def isIsomorphic(s, t):
+    """
+    :type s: str
+    :type t: str
+    :rtype: bool
+    """
+    if len(s) != len(t):
+        return False
+    
+    sPlacements = collections.defaultdict(list)
+    tPlacements = collections.defaultdict(list)
+    for i, v in enumerate(s):
+        sPlacements[v].append(i)
+    for j, w in enumerate(t):
+        tPlacements[w].append(j)
+        
+    print(sPlacements.values())
+    print(tPlacements.values())
+    
+    for k in sPlacements.values():
+        if k not in tPlacements.values():
+            return False
+    
+    return True
+        
+        
