5afee8922f451486b5b06f460ac2e3dad2cd1da3
Vincent Teng <vincentteng@lyft.com>
2020-07-04 12:37:39 -0400

    Added ugly num 2 from LC

---
+++ lc_ugly_num_ii.py
@@ -0,0 +1,24 @@
+# https://leetcode.com/explore/challenge/card/july-leetcoding-challenge/544/week-1-july-1st-july-7th/3380/
+# Had to peek answer
+
+
+class Ugly:
+    def __init__(self):
+        self.nums = nums = [1, ]
+        i2 = i3 = i5 = 0
+
+        for i in range(1, 1690):
+            ugly = min(nums[i2] * 2, nums[i3] * 3, nums[i5] * 5)
+            nums.append(ugly)
+
+            if ugly == nums[i2] * 2: 
+                i2 += 1
+            if ugly == nums[i3] * 3:
+                i3 += 1
+            if ugly == nums[i5] * 5:
+                i5 += 1
+            
+class Solution:
+    u = Ugly()
+    def nthUglyNumber(self, n):
+        return self.u.nums[n - 1]
