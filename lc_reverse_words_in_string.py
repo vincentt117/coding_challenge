a28091f512538c33735d9b2db512fbf587dfb438
Vincent Teng <vincentteng@lyft.com>
2020-07-15 19:32:26 -0400

    Added another solution for the question

--- lc_reverse_words_in_string.py
+++ lc_reverse_words_in_string.py
@@ -9,4 +9,17 @@ def reverseWords(self, s):
     return ' '.join(s[::-1])
 
 # Faster than 99% of submissions
+
+# Solution from July 15, 2020
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        words = s.split(' ')
+        ret = ''
+        for i in range(len(words) - 1, -1, -1):
+            word = words[i]
+            if len(word) > 0:
+                ret += word + ' '
+        return ret[:-1]
+                
+        
     
